3c840bfe95a496d99e1875e6e720e9d5f080f047
Arhaan Khaku <113068699+arhaankk@users.noreply.github.com>
2025-03-01 13:33:55 -0800

    Add profile page
    Co-authored-by: Bradan Fleming <bradan@cogspin.com>

---
+++ scripts/profile.js
@@ -0,0 +1,51 @@
+document.addEventListener("DOMContentLoaded", function () {
+	const editButton = document.getElementById("edit-button");
+	const saveButton = document.getElementById("save-button");
+	const inputs = document.querySelectorAll(".login__input");
+	const form = document.querySelector('form');
+
+	let validateForm = () => {
+		let valid = true;
+		let err = '';
+		/* Dirty reimplementation of HTML checks */
+		for (i of inputs) {
+			if (i.value.length === 0 && i.hasAttribute('required')) {
+				i.classList.add('aria-invalid');
+				valid = false;
+				err = `The ${i.name} is required.`;
+			} else if (i.type === 'number' && parseInt(i.value,10).toString() !== i.value) {
+				i.classList.add('aria-invalid');
+				valid = false;
+				err = `The ${i.name} must be a number.`;
+			} else if (i.type === 'email' && i.value.indexOf('@') < 0) {
+				i.classList.add('aria-invalid');
+				valid = false;
+				err = `The ${i.name} must be a valid email.`;
+			} else {
+				i.classList.remove('aria-invalid');
+			}
+		}
+		const f = form.elements;
+		if (parseInt(f['age'].value) < 18 || parseInt(f['age'].value) > 150) {
+			err = 'Invalid age.'
+			valid = false;
+		}
+		if (!valid)
+			alert(err);
+		return valid;
+	}
+
+	editButton.addEventListener("click", function () {
+		inputs.forEach(input => input.removeAttribute("disabled"));
+		saveButton.style.display = "block";
+		editButton.style.display = "none";
+	});
+
+	saveButton.addEventListener("click", function () {
+		if (!validateForm())
+			return;
+		inputs.forEach(input => input.setAttribute("disabled", true));
+		saveButton.style.display = "none";
+		editButton.style.display = "block";
+	});
+});
