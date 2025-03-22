d490b0fb7f76fd062e7877b4c3c778e6469968f1
Bradan Fleming <bradan@cogspin.com>
2025-03-22 16:47:02 -0700

    Fix tabs in index page

--- pages/index.php
+++ pages/index.php
@@ -8,29 +8,29 @@ $page->preamble();
 ?>
 
 <main>
-    <h1>iBlog</h1>
-    <p>Welcome to my iBlog!</p>
+	<h1>iBlog</h1>
+	<p>Welcome to my iBlog!</p>
 
-    <?php if (!$session->isAuthenticated()) { ?>
-    <a href="<?php echo $page->data('pages'); ?>/signin.php" class="button">Log In</a>
-    <?php } else { ?>
-    <a href="<?php echo $page->data('pages'); ?>blog-write.html" class="button">Write Post</a>
-    <?php } ?>
-    <hr>
-    <!-- TODO: Replace placeholder -->
-    <h2>Recent activity</h2>
-        <div class="card--mini">
-            <img src="../images/profilePic.jpg" alt="User1 Profile Picture" class="post-avatar">
-            <strong class="post-user-name">Cynthia: </strong>
-            <h2>Blog Title 1</h2>
-            <div class="blog-content-text">
-                <p class="text--preview">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-                </p>
-            </div>
-            <div>
+	<?php if (!$session->isAuthenticated()) { ?>
+	<a href="<?php echo $page->data('pages'); ?>/signin.php" class="button">Log In</a>
+	<?php } else { ?>
+	<a href="<?php echo $page->data('pages'); ?>blog-write.html" class="button">Write Post</a>
+	<?php } ?>
+	<hr>
+	<!-- TODO: Replace placeholder -->
+	<h2>Recent activity</h2>
+		<div class="card--mini">
+			<img src="../images/profilePic.jpg" alt="User1 Profile Picture" class="post-avatar">
+			<strong class="post-user-name">Cynthia: </strong>
+			<h2>Blog Title 1</h2>
+			<div class="blog-content-text">
+				<p class="text--preview">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+				</p>
+			</div>
+			<div>
 				<a href="blogCard.html" class="button">Full Post</a>
-            </div>
-        </div>
+			</div>
+		</div>
 </main>
 
 <?php $page->epilogue(); ?>
